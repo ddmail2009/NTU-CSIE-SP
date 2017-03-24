7316e4d3737a5c0e5130ca699ba4fa60d0af9255
Po-Hsien Wang <ddmail2009@gmail.com>
2017-03-23 23:14:16 -0700

    structure change

--- File_processing/library.h
+++ File_processing/library.h
@@ -1,9 +1,9 @@
 #define MAX_TITLE_LENGTH (32)
 #define MAX_RESERVE (16)
 struct BookInfo{
-	char title[MAX_TITLE_LENGTH];
-	int number_of_resv_req; /*>=0, <MAX_RESERVE*/
-	int userID_for_resv_request[MAX_RESERVE];
+    char title[MAX_TITLE_LENGTH];
+    int number_of_resv_req; /*>=0, <MAX_RESERVE*/
+    int userID_for_resv_request[MAX_RESERVE];
 };
 #define NO_USER (-1)
 /*userID_for_resv_request[0] is either NO_USER or integer >=0*/
